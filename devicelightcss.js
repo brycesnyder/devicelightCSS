b779a33531962ef0c03cac0dfc70292fe8d9886b
Bryce Snyder <bryce.snyder@hotmail.com>
2016-11-04 13:54:22 -0400

    Figured out how to swap CSS files.

--- devicelightcss.js
+++ devicelightcss.js
@@ -1,17 +1,21 @@
-window.addEventListener('devicelight', function(e) {
-	var threshold = 50;
-	//event.value is the lux value returned by the sensor on the device
-	(e.value < threshold) ? swapSheet('dark', 'light') : swapSheet('light', 'dark');
+var threshold = 50;
+var state = {
+	to: 'light',
+	from: 'dark'
+}
 
+window.addEventListener('devicelight', function(e) {
+	//event.value is the lux value returned by the sensor on the device
+	(e.value < threshold) ? swapSheet() : swapSheet();
 });
 
 // @TODO need to get current filepath of stylesheets
-// @TODO need to detemine to & fro swap
-var swapSheet = function(to, fro) {
+var swapSheet = function() {
+	state.to = state.from + (state.from = state.to, "");
 	var stylesheets = document.querySelectorAll('link[rel=stylesheet]');
 	for (var i = 0; i < elements.length; i++) {
-		if (stylesheets[i].href.indexOf(fro) !== -1) {
-			stylesheets[i].setAttribute('href', './css/' + to + '.css');
+		if (stylesheets[i].href.indexOf(state.from) !== -1) {
+			stylesheets[i].setAttribute('href', './css/' + state.to + '.css');
 		}
 	}
 };
